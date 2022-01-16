b2ca7049643161e4899730c2f1d7ea7038467422
casudin <casudin.ahi@gmail.com>
2022-01-16 22:15:05 +0700

    add new branch

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -51,6 +51,16 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        ],
+        's3_costum' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID_2'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY_2'),
+            'region' => env('AWS_DEFAULT_REGION_2'),
+            'bucket' => env('AWS_BUCKET_2'),
+            'url' => env('AWS_URL_2'),
+            'endpoint' => env('AWS_ENDPOINT_2'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT_2', false),
         ],
 
     ],
